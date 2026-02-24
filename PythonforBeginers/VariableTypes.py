c66aabb710fb9cca6dc8180ad9935637f4e2a127
Sanjit Sarkar <sanjit.est14@gmail.com>
2026-02-24 22:53:36 +0600

    Variable Types

---
+++ PythonforBeginers/VariableTypes.py
@@ -0,0 +1,12 @@
+integer_type = 5
+float_type = 4.4
+bool_type = True
+string_type = "Sanjit"
+list_type = [3, "Sanjit", True]
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
