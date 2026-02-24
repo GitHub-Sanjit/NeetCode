2a0a5a7edafac11d72d8ee4a255067dd0885de91
Sanjit Sarkar <sanjit.est14@gmail.com>
2026-02-24 22:58:09 +0600

    Dynamic Typing

---
+++ PythonforBeginers/DynamicTyping.py
@@ -0,0 +1,10 @@
+variable = 5
+print(type(variable))
+variable = 5.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Sanjit"
+print(type(variable))
+variable = []
+print(type(variable))
